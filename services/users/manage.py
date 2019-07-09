213294aefaed2df6dc302beaea58e76560b96472
Jon Yu <jy900x2@gmail.com>
2019-07-08 22:29:07 -0400

    refactor flask organization

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,14 +1,16 @@
 # services/users/manage.py
 
+
 import sys
 import unittest
 
 from flask.cli import FlaskGroup
 
-from project import app, db
+from project import create_app, db   # new
+from project.api.models import User  # new
 
-
-cli = FlaskGroup(app)
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
 
 
 @cli.command('recreate_db')
@@ -16,6 +18,7 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
 
 @cli.command()
 def test():
